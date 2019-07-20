cb63ab8bd4435dc107c7b5d48584c3ea07e1c4a8
gshields5541 <macbook@MacBooks-MacBook-Pro.local>
2019-07-20 16:42:05 -0700

    configuring the travis.yml file

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export default {
-    AWS_LAMBDA_GETSIGNEDURL_ENDPOINT: 'http://myphotobin.s3-website-sa-east-1.amazonaws.com'
+    AWS_LAMBDA_GETSIGNEDURL_ENDPOINT: 'http://ad440uploadhost.s3-website-us-west-2.amazonaws.com'
   }
