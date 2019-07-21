1a4d53d0077758e42a4b6de59f0715d2038be5a2
gshields5541 <macbook@MacBooks-MacBook-Pro.local>
2019-07-21 13:59:36 -0700

    Change endpoint again

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export default {
-    AWS_LAMBDA_GETSIGNEDURL_ENDPOINT: 'https://oir526jvq6.execute-api.sa-east-1.amazonaws.com/default/getSignedURL'
+    AWS_LAMBDA_GETSIGNEDURL_ENDPOINT: 'https://o8xcl49usg.execute-api.us-west-2.amazonaws.com/prod/myUploader'
   }
