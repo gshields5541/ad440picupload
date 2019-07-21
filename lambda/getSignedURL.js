eedcc1bc840d0b912b71b68f1cfdc1e426cfce4d
gshields5541 <macbook@MacBooks-MacBook-Pro.local>
2019-07-21 15:22:31 -0700

    Changed getSignedURL.js file to Node 10 version

--- lambda/getSignedURL.js
+++ lambda/getSignedURL.js
@@ -2,27 +2,61 @@ var AWS = require('aws-sdk');
 var s3 = new AWS.S3();
 var bucketName = process.env.AWS_BUCKET_NAME;
 
-exports.handler = (event, context) => {
-    if (!event.hasOwnProperty ('contentType')) {
-        context.fail({ err: 'Missing contentType' })
+exports.handler = async (event) => {
+    let payload = {};
+    if (event.body) {
+        payload = JSON.parse(event.body);
     }
-
-    if (!event.hasOwnProperty('filePath')) {
-        context.fail({ err: 'Missing filePath'})
-    }
-
-    var params = {
+    const params = {
         Bucket: bucketName,
-        Key: event.filePath,
+        Key: payload.filePath,
         Expires: 3600,
-        ContentType: event.contentType
+        ContentType: payload.contentType
+    };
+    const response = {
+        statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": true,
+            "Access-Control-Allow-Headers": 'content-type',
+        },
+        body: {},
+    };
+    if (!payload.hasOwnProperty('contentType')) {
+        response.body = JSON.stringify({
+            err: 'Missing contentType'
+        });
+        return response;
     }
 
-    s3.getSignedUrl('putObject', params, (err, url) => {
-        if (err) {
-            context.fail({ err })
-        } else {
-            context.succeed({ url })
-        }
-    })
-}
+    if (!payload.hasOwnProperty('filePath')) {
+        response.body = JSON.stringify({
+            err: 'Missing filePath'
+        });
+        return response;
+    }
+
+    try {
+        const url = await _getUrl(params);
+        response.body = JSON.stringify({
+            url
+        });
+    } catch (err) {
+        response.body = JSON.stringify({
+            err: err
+        });
+    }
+    return response;
+
+    function _getUrl(params) {
+        return new Promise((resolve, reject) => {
+            s3.getSignedUrl('putObject', params, (err, url) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(url);
+                }
+            });
+        });
+    }
+};
