c18b583d38da0b0c56d43fccb1e0ba20e5b4450d
gshields5541 <gshieldssea5541@gmail.com>
2019-08-01 16:28:13 -0700

    deploying recent changes

--- lambda/getSignedURL.js
+++ lambda/getSignedURL.js
@@ -1,62 +1,28 @@
-var AWS = require('aws-sdk');
+var AWS = require('aws-sdk')
 var s3 = new AWS.S3();
-var bucketName = process.env.AWS_BUCKET_NAME;
+var bucketName = process.env.AWS_BUCKET_NAME
 
-exports.handler = async (event) => {
-    let payload = {};
-    if (event.body) {
-        payload = JSON.parse(event.body);
+exports.handler = (event, context) => {
+    if (!event.hasOwnProperty('contentType')) {
+        context.fail({ err: 'Missing contentType' })
     }
-    const params = {
+
+    if (!event.hasOwnProperty('filePath')) {
+        context.fail({ err: 'Missing filePath' })
+    }
+
+    var params = {
         Bucket: bucketName,
-        Key: payload.filePath,
+        Key: event.filePath,
         Expires: 3600,
-        ContentType: payload.contentType
-    };
-    const response = {
-        statusCode: 200,
-        headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Credentials": true,
-            "Access-Control-Allow-Headers": 'content-type',
-        },
-        body: {},
-    };
-    if (!payload.hasOwnProperty('contentType')) {
-        response.body = JSON.stringify({
-            err: 'Missing contentType'
-        });
-        return response;
+        ContentType: event.contentType
     }
 
-    if (!payload.hasOwnProperty('filePath')) {
-        response.body = JSON.stringify({
-            err: 'Missing filePath'
-        });
-        return response;
-    }
-
-    try {
-        const url = await _getUrl(params);
-        response.body = JSON.stringify({
-            url
-        });
-    } catch (err) {
-        response.body = JSON.stringify({
-            err: err
-        });
-    }
-    return response;
-
-    function _getUrl(params) {
-        return new Promise((resolve, reject) => {
-            s3.getSignedUrl('putObject', params, (err, url) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(url);
-                }
-            });
-        });
-    }
-};
+    s3.createPresignedPost('putObject', params, (err, url) => {
+        if (err) {
+            context.fail({ err })
+        } else {
+            context.succeed({ url })
+        }
+    })
+}
--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export default {
-    AWS_LAMBDA_GETSIGNEDURL_ENDPOINT: 'https://o8xcl49usg.execute-api.us-west-2.amazonaws.com/prod/myUploader'
+    AWS_LAMBDA_GETSIGNEDURL_ENDPOINT: 'https://a9m5syby7i.execute-api.us-east-2.amazonaws.com/prod/getSignedURL'
   }
